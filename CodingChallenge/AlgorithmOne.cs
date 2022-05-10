7788ea56c6940440370129e71200af16d2be0745
bogdancanciu <bogdancanciu21@gmail.com>
2022-05-10 12:32:11 +0300

    added implementation for algorithm one

--- CodingChallenge/AlgorithmOne.cs
+++ CodingChallenge/AlgorithmOne.cs
@@ -9,5 +9,22 @@ namespace CodingChallenge
     public class AlgorithmOne : IAlgoInt
     {
         //Algorithm implementation meant to solve Problem 1 proposed by mihaigpm in his algorithms-codingchallenge repo.
+        private int[] inputArray;
+        public AlgorithmOne(int[] arr)
+        {
+            inputArray = arr;
+        }
+        public int Solve()
+        {
+            int sum = 0;
+            foreach (int number in inputArray)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
     }
 }
